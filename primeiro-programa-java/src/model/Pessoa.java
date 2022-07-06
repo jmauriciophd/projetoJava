e7f3367c6279ad4f478e93fbd0ce694440c73736
Mauricio Gomes <jmauriciophd@gmail.com>
2022-07-06 15:42:33 -0300

    Novo commit

--- primeiro-programa-java/src/model/Pessoa.java
+++ primeiro-programa-java/src/model/Pessoa.java
@@ -8,4 +8,32 @@ public class Pessoa {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the sobrenome
+	 */
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	/**
+	 * @param sobrenome the sobrenome to set
+	 */
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
 }
