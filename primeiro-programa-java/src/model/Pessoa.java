cd0bcebbda049a6c3ad5b2964d44436e34db7fe8
Mauricio Gomes <jmauriciophd@gmail.com>
2022-07-07 09:54:27 -0300

    Adicionado campo dtNascimento

--- primeiro-programa-java/src/model/Pessoa.java
+++ primeiro-programa-java/src/model/Pessoa.java
@@ -1,9 +1,14 @@
 package model;
 
+import java.sql.Date;
+
 public class Pessoa {
 	private String nome;
 	private String sobrenome;
 	private int idade;
+	
+	private Date dtNascimento;
+	
 	public Pessoa() {
 		// TODO Auto-generated constructor stub
 	}
@@ -48,6 +53,14 @@ public class Pessoa {
 	 */
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
 	}
 	
 	
